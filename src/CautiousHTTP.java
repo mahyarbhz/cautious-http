e58d2fe3490b53fc3118430891739544e0454dc7
MahyarBHZ <mahyarbhz@gmail.com>
2025-04-16 16:26:28 +0330

    added some request handling

--- src/CautiousHTTP.java
+++ src/CautiousHTTP.java
@@ -21,19 +21,38 @@ public class CautiousHTTP {
     }
 
     private static void handleRequest(Socket clientSocket) {
-        try (InputStream in = clientSocket.getInputStream()) {
-            System.out.println("Raw data (bytes):");
-            byte[] buffer = new byte[128];
-            int bytesRead;
-            while ((bytesRead = in.read(buffer)) != -1) {
-                // Print bytes in HEX format
-                for (int i = 0; i < bytesRead; i++) {
-                    System.out.printf("%02X ", buffer[i]); // e.g., "48 65 6C 6C 6F" = "Hello"
-                }
-                System.out.println();
-            }
+//        try (InputStream in = clientSocket.getInputStream()) {
+//            System.out.println("Raw data (bytes):");
+//            byte[] buffer = new byte[128];
+//            int bytesRead;
+//            while ((bytesRead = in.read(buffer)) != -1) {
+//                // Print bytes in HEX format
+//                for (int i = 0; i < bytesRead; i++) {
+//                    System.out.printf("%02X ", buffer[i]); // e.g., "48 65 6C 6C 6F" = "Hello"
+//                }
+//                System.out.println();
+//            }
+//        } catch (IOException e) {
+//            System.err.println("Server error: " + e.getMessage());
+//        }
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+             OutputStream out = clientSocket.getOutputStream()) {
+
+            // Read request
+            String requestLine = in.readLine();
+            System.out.println("Received request: " + requestLine);
+            String nextLine = in.readLine();
+            System.out.println("Next line: " + nextLine);
+
+            // Prepare response,
+            // that \r\n\r defines that it is the end of the headers and
+            // the body is being sent as the rest of the response.
+            String response = "HTTP/1.1 200 OK\r\n\r\nHello World!";
+            out.write(response.getBytes());
+            out.flush();
+
         } catch (IOException e) {
-            System.err.println("Server error: " + e.getMessage());
+            System.err.println("Request handling error: " + e.getMessage());
         }
     }
 }
